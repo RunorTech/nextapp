9e65a0c55d319812af583747b5a39fca7287d324
RunorTech <ighorodjeoghenerunor@gmail.com>
2024-09-02 22:05:36 +0200

    blog

---
+++ app/(root)/blog/components/Blog.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <div className='blog-card'>
+            <img src="" alt="" />
+            <div className="blog-content">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit similique architecto dolorum vitae inventore nobis quis dignissimos voluptas labore amet, consequuntur at reiciendis perspiciatis possimus quae laborum, repellat voluptatum ad.
+            </div>
+
+        </div>
+    )
+}
+
+export default Blog
--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Blog from './components/Blog'
 
 const BlogHomePage = () => {
     return (
         <main>
             <Hero />
+            <Blog/>
         </main>
     )
 }
