1e52becf02a3cdfd61732f1457a1d4ad513c3719
RunorTech <ighorodjeoghenerunor@gmail.com>
2024-09-02 17:01:09 +0200

    side bar

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -3,33 +3,35 @@ import React from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import CloseIcon from '@mui/icons-material/Close';
+import { toggleMenu } from '@/lib/utils';
 
 const NavLinks = () => {
-    const pathname = usePathname();
-
-    const toggleMenu = () => {
-      console.log("menu")
-    }
+  const pathname = usePathname();
+  toggleMenu();
 
   return (
     <nav className='nav-bar'>
-       <div className="desktop-nav">
+      <div className="desktop-nav">
         <div className="nav-logo">
           TECH LOGO
         </div>
         <div className="nav-links">
-        <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">home</Link>
+          <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">home</Link>
         </div>
         <div className="nav-icon" onClick={toggleMenu}>
-        <MenuOpenIcon style={{width: 40, height: 40}} />
+          <MenuOpenIcon style={{ width: 40, height: 40 }} />
         </div>
-       </div>
-        <div className="mobile-nav">
-          <div className="side-slide">
-
-          </div>
+      </div>
+      <div className="mobile-nav" id='mobile-nav'>
+        <div className="close-icon" onClick={toggleMenu}>
+          <CloseIcon />
+        </div>
+        <div className="side-slide">
 
         </div>
+
+      </div>
     </nav>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const toggleMenu = () => {
+  const navMenu: HTMLElement | null = document.getElementById('mobile-nav');
+  console.log(navMenu)
+  navMenu?.classList.toggle("show-menu");
+
+}
