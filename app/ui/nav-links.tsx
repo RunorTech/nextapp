1bad9148e76c6a8bbceda34da58f7abe0e6fbc92
RunorTech <ighorodjeoghenerunor@gmail.com>
2024-09-02 15:58:33 +0200

    menu

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -2,12 +2,34 @@
 import React from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 
 const NavLinks = () => {
     const pathname = usePathname();
+
+    const toggleMenu = () => {
+      console.log("menu")
+    }
+
   return (
     <nav className='nav-bar'>
+       <div className="desktop-nav">
+        <div className="nav-logo">
+          TECH LOGO
+        </div>
+        <div className="nav-links">
         <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">home</Link>
+        </div>
+        <div className="nav-icon" onClick={toggleMenu}>
+        <MenuOpenIcon />
+        </div>
+       </div>
+        <div className="mobile-nav">
+          <div className="side-slide">
+
+          </div>
+
+        </div>
     </nav>
   )
 }
