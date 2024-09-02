be5b27c57d4859c41087ffb5a08be4b66e00f718
RunorTech <ighorodjeoghenerunor@gmail.com>
2024-09-02 05:18:16 +0200

    still seting up

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (   
  <main>
-
+hello how are you doing i hope all is fine and you are  living your best live
  </main>
   );
 }
