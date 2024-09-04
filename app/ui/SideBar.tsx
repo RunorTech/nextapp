2871d8dc0e731df303ad57721ab007da7bf99de2
RunorTech <ighorodjeoghenerunor@gmail.com>
2024-09-04 09:55:21 +0200

    dark mode

--- app/ui/SideBar.tsx
+++ app/ui/SideBar.tsx
@@ -24,14 +24,14 @@ export function SidebarDemo() {
       label: "Home",
       href: "/",
       icon: (
-        <HomeIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <HomeIcon className="my-main-text h-5 w-5 flex-shrink-0" />
       ),
     },
     {
       label: "Developer profile",
       href: "#",
       icon: (
-        <CodeRoundedIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <CodeRoundedIcon className="my-main-text h-5 w-5 flex-shrink-0" />
       
       ),
     },
@@ -39,21 +39,21 @@ export function SidebarDemo() {
       label: "Use Note project",
       href: "#",
       icon: (
-        <EditNoteRoundedIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <EditNoteRoundedIcon className="my-main-text h-5 w-5 flex-shrink-0" />
       ),
     },
     {
       label: "Job Request",
       href: "#",
       icon: (
-        <SendRoundedIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <SendRoundedIcon className="my-main-text h-5 w-5 flex-shrink-0" />
       ),
     },
     {
       label: "Blog",
       href: "/blog",
       icon: (
-        <BookIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <BookIcon className="my-main-text h-5 w-5 flex-shrink-0" />
       ),
     },
   ];
@@ -68,7 +68,7 @@ export function SidebarDemo() {
       <Sidebar open={open} setOpen={setOpen}>
         <SidebarBody className="justify-between gap-10">
           <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-            {open ? <><LogoIcon/> RUNOR</> : <LogoIcon /> }
+            {open ? <><Logo/></> : <LogoIcon /> }
             <div className="mt-8 flex flex-col gap-2">
               {links.map((link, idx) => (
                 <SidebarLink key={idx} link={link} />
@@ -104,13 +104,13 @@ export const Logo = () => {
       href="#"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
-      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <div className="h-5 w-6 icon-menu-logo rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
       <motion.span
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className="font-medium text-black dark:text-white whitespace-pre"
+        className="font-medium my-main-text whitespace-pre"
       >
-        Acet Labs
+        RUNOR
       </motion.span>
     </Link>
   );
@@ -121,7 +121,7 @@ export const LogoIcon = () => {
       href="#"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
-      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <div className="h-5 w-6 icon-menu-logo rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
     </Link>
   );
 };
