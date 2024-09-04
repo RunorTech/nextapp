15f10a3344979c8b858957131b94c6cdf32b79ea
RunorTech <ighorodjeoghenerunor@gmail.com>
2024-09-04 20:01:17 +0200

    check

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,4 @@
 
-import Image from "next/image";
-import { redirect } from "next/navigation";
-import { NavbarDemo } from "../ui/NavBar";
 import { SidebarDemo } from "../ui/SideBar";
 
 export default async function RootLayout({
